39e5ea8b9052326eb35ff9c2e4206baa76d5542b
Christian <git@fiae.ws>
2014-05-28 20:44:12 +0200

    foo

--- src/eu/anynet/anybot/BotThread.java
+++ src/eu/anynet/anybot/BotThread.java
@@ -109,6 +109,10 @@ public class BotThread extends Thread {
                }
             }
          }
+         else
+         {
+            me.writePipeLine("No modules found.");
+         }
 
 
 
